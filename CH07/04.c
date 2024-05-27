e023cd8ce01391edfa6fd113e8bc775bcd242735
주은 <168528163+zooeunii@users.noreply.github.com>
2024-05-27 16:11:56 +0900

    04.c

--- CH07/04.c
+++ CH07/04.c
@@ -5,10 +5,10 @@ int main()
 {
 	int x, i;
 
-	printf("Á¤¼ö¸¦ ÀÔ·ÂÇÏ½Ã¿À: ");
+	printf("ì •ìˆ˜ë¥¼ ìž…ë ¥í•˜ì‹œì˜¤: ");
 	scanf("%d", &x);
 
-	printf("¾à¼ö: ");
+	printf("ì•½ìˆ˜: ");
 	for (i = 1;i <= x;i++)
 		if (x % i == 0)
 			printf("%d ", i);
